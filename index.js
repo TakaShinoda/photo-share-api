67ccea5efc9aa6df7f5b15688a6d3500853e08b1
Takahiro <tkhr_621@docomo.ne.jp>
2020-03-16 22:35:20 +0900

    ApolloServerモジュールの読み込みを追加

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+// apollo-serverモジュールの読み込み
+const {ApolloServer} = require('apollo-server')
+
 const typeDefs = `
     type Query {
         totalPhoto: Int!
     }
 `
+
+// リゾルバはスキーマの定義を満たす
+// スキーマで定義されたフィールド名と同じ名前を持ち，スキーマで定義されたデータ型の結果を返す
 const resolvers = {
     Query: {
         totalPhoto: () => 42
     }
 }
+
+// サーバーのインスタンス作成
+// その際，typeDefs(スキーマ)とリゾルバを引数に取る
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+// webサーバの起動
+// サーバーに対してlisten関数を実行することで、httpサーバーが待ち受けを開始
+server
+    .listen()
+    .then(({url}) => Console.log('GraphQL Server running on ${url}'))
